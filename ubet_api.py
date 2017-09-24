51ab85f103e4d233b668b544750c5c25a5ad887b
Mortalcatalyst <flebber.crue@gmail.com>
2017-09-24 16:18:58 +1000

    first commit

---
+++ ubet_api.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import pprint
+# import tablib
+
+
+r = requests.get(
+    "https://api.tatts.com/sales/vmax/web/data/racing/2017/09/16/SR/full")
+pp = pprint.PrettyPrinter(indent=2)
+data = r.json()
+# pp.pprint(data
+# attrs = ['Number','NameForm','TrackRating','Distance']
+# for item in data["RaceDay"]['Meetings'][0]:
+#     for att in attrs:
+#         print(item[att])
+
+# pp.pprint(data["RaceDay"]['Meetings'][0]['Races'][0]['FixedPriceSummary']['FixedPrices'][0]['MeetingId'])
+# pp.pprint(data["RaceDay"]['Meetings'][0]['Races'][0]['RacingFormGuide']['Event']['Race'])
+# ['Runners']['RacingFormGuide'])
+
+# raceItems = ['Name', 'Number', 'Distance']
+
+raceData = []
+
+for item in data["RaceDay"]['Meetings'][0]['Races']:
+    raceDetails = item['RacingFormGuide']['Event']['Race']
+    raceData.append((raceDetails['Name'],raceDetails['Number'],raceDetails['Distance']))
+    # raceData.extend(raceDetails['Name'],raceDetails['Number'],raceDetails['Distance'])
+
+print(raceData)
+# headers = ('Name', 'Number', 'Distance')
+# data = (raceDetails['Name'],raceDetails['Number'],raceDetails['Distance'])
+# data = tablib.Dataset(*data, headers=headers)
+
+
+    # print("Race called {0} over {1} and its race number {2}.".format(raceDetails['Name'],
+    #                                                                  raceDetails['Distance'],
+    #                                                                  raceDetails['Number']))
